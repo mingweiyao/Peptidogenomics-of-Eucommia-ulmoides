4eb553f0082e03339a7fac2003fa41191a37ec49
mingweiyao <yaomingwei0318@163.com>
2026-01-18 00:43:40 +0800

    merge

--- .history/new_code/figure4_20260118003016.py
+++ .history/new_code/figure4_20260118003016.py
@@ -1,25 +1,3 @@
-# # 根据group提取并筛选表达的sp
-# import pandas as pd
-# gene_expression_df = pd.read_csv(r"F:\work_mechanism\new_file\01location\rnaseq\03ouput\finally_expressed_sp_cpm.csv", index_col=0)
-# tissue_mapping_df = pd.read_excel(r"D:\Desktop\Total_rna_seq.xlsx", sheet_name="extract")
-# tissue_group_to_samples = (tissue_mapping_df.groupby(['Tissues', 'Group'])['Sample'].apply(list).to_dict())
-# peptide_ids_by_tissue = {}
-# for (tissue, group), samples in tissue_group_to_samples.items():
-#     samples = [s for s in samples if s in gene_expression_df.columns]
-#     group_expr = gene_expression_df.loc[:, samples]
-#     group_mask = (group_expr >= 1).all(axis=1)
-#     if tissue not in peptide_ids_by_tissue:
-#         peptide_ids_by_tissue[tissue] = set()
-#     peptide_ids_by_tissue[tissue].update(group_expr.index[group_mask].tolist())
-# peptide_ids_by_tissue = {tissue: list(ids) for tissue, ids in peptide_ids_by_tissue.items()}
-# max_length = max((len(ids) for ids in peptide_ids_by_tissue.values()), default=0)
-# peptide_ids_df = pd.DataFrame({
-#     tissue: ids + [None] * (max_length - len(ids))
-#     for tissue, ids in peptide_ids_by_tissue.items()
-# })
-# output_excel_path = r"D:\Desktop\rnaseq_veen_leaf_location.xlsx"
-# peptide_ids_df.to_excel(output_excel_path, index=False)
-
 # # 转录组数据基础的NCP分布veen图
 # import pandas as pd
 # gene_expression_df = pd.read_csv(r"F:\work_mechanism\new_file\01location\rnaseq\03ouput\finally_expressed_sp_cpm.csv", index_col=0)
@@ -581,6 +559,28 @@
 # output_gff = r"F:\work_mechanism\new_file\02figure\figure4\sp_codon.gff"
 # df = pd.read_excel(excel_file, sheet_name="unique")
 # excel_to_gff3(df, output_gff)
+
+# # 根据group提取并筛选表达的sp
+# import pandas as pd
+# gene_expression_df = pd.read_csv(r"F:\work_mechanism\new_file\01location\rnaseq\03ouput\finally_expressed_sp_cpm.csv", index_col=0)
+# tissue_mapping_df = pd.read_excel(r"D:\Desktop\Total_rna_seq.xlsx", sheet_name="extract")
+# tissue_group_to_samples = (tissue_mapping_df.groupby(['Tissues', 'Group'])['Sample'].apply(list).to_dict())
+# peptide_ids_by_tissue = {}
+# for (tissue, group), samples in tissue_group_to_samples.items():
+#     samples = [s for s in samples if s in gene_expression_df.columns]
+#     group_expr = gene_expression_df.loc[:, samples]
+#     group_mask = (group_expr >= 1).all(axis=1)
+#     if tissue not in peptide_ids_by_tissue:
+#         peptide_ids_by_tissue[tissue] = set()
+#     peptide_ids_by_tissue[tissue].update(group_expr.index[group_mask].tolist())
+# peptide_ids_by_tissue = {tissue: list(ids) for tissue, ids in peptide_ids_by_tissue.items()}
+# max_length = max((len(ids) for ids in peptide_ids_by_tissue.values()), default=0)
+# peptide_ids_df = pd.DataFrame({
+#     tissue: ids + [None] * (max_length - len(ids))
+#     for tissue, ids in peptide_ids_by_tissue.items()
+# })
+# output_excel_path = r"D:\Desktop\rnaseq_veen_leaf_location.xlsx"
+# peptide_ids_df.to_excel(output_excel_path, index=False)
 
 # # TPM标准化
 # import os
